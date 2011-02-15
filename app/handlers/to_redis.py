d84fb1ff19a1bd7e813a39f11364732ae5b936ff
Roman Khomenko <none@none>
2011-02-15 13:34:00 +0200

    fix

--- app/handlers/to_redis.py
+++ app/handlers/to_redis.py
@@ -5,33 +5,8 @@ import time
 import redis
 from lamson.routing import route, stateless
 
-import logging
-
 class Transform(object):
-    """Abstraction for a regular expression transform.
-
-    Transform subclasses have two properties:
-       regexp: the regular expression defining what will be replaced
-       replace(MatchObject): returns a string replacement for a regexp match
-
-    We iterate over all matches for that regular expression, calling replace()
-    on the match to determine what text should replace the matched text.
-
-    The Transform class is more expressive than regular expression replacement
-    because the replace() method can execute arbitrary code to, e.g., look
-    up a WikiWord to see if the page exists before determining if the WikiWord
-    should be a link.
-    """
-
     def run(self, content):
-        """Runs this transform over the given content.
-
-        Args:
-          content: The string data to apply a transformation to.
-
-        Returns:
-          A new string that is the result of this transform.
-        """
         parts = []
         offset = 0
         for match in self.regexp.finditer(content):
@@ -43,8 +18,6 @@ class Transform(object):
 
 
 class AutoLink(Transform):
-    """A transform that auto-links URLs."""
-
     def __init__(self):
         self.regexp = re.compile(r'([^"])\b((http|https)://[^ \t\n\r<>\(\)&"]+'\
                                  r'[^ \t\n\r<>\(\)&"\.])')
